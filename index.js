728e9bc78bb10d3f7e52da939a4bae62b8851b2c
ralphtheninja <ralphtheninja@riseup.net>
2015-01-16 17:31:08 +0100

    add sync version

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ function readlink(link, cb) {
   async.eachSeries(sub_paths, function (sub_path, next) {
     expandPath(path.join(result, sub_path), function (err, next_path) {
       if (err) return next(err)
-      result = next_path
+      result = path.resolve(result, next_path)
       next()
     })
   }, function (err) {
     cb(err, result)
   })
+}
+
+readlink.sync = function (link) {
+  var result = '/'
+  if (link[0] !== '/') link = path.join(process.cwd(), link)
+  var sub_paths = link.split('/').splice(1)
+  sub_paths.forEach(function (sub_path) {
+    result = path.resolve(result, expandPathSync(path.join(result, sub_path)))
+  })
+  return result
 }
 
 function expandPath(path, cb) {
@@ -25,6 +35,13 @@ function expandPath(path, cb) {
     }
     cb(null, path)
   })
+}
+
+function expandPathSync(path) {
+  if (fs.lstatSync(path).isSymbolicLink()) {
+    return fs.readlinkSync(path)
+  }
+  return path
 }
 
 module.exports = readlink
--- test/test.js
+++ test/test.js
@@ -9,10 +9,14 @@ var readlink = require('../')
 test('non existing path errors', function (t) {
   readlink('/this/path/does/not/exist', function (err, result) {
     t.ok(err)
-    t.equal(err.errno, 34)
     t.equal(err.code, 'ENOENT')
     t.end()
   })
+})
+
+test('non existing path throws (sync)', function (t) {
+  t.throws(function () { readlink.sync('/this/path/does/not/exist') }, /ENOENT/)
+  t.end()
 })
 
 test('existing folder expands to self', function (t) {
@@ -25,6 +29,14 @@ test('existing folder expands to self', function (t) {
   })
 })
 
+test('existing folder expands to self (sync)', function (t) {
+  tmpdir(function (err, dir) {
+    t.ok(!err)
+    t.equal(readlink.sync(dir), dir)
+    t.end()
+  })
+})
+
 test('existing file expands to self', function (t) {
   tmp.file(function (err, file) {
     t.ok(!err)
@@ -32,6 +44,14 @@ test('existing file expands to self', function (t) {
       t.equal(result, file)
       t.end()
     })
+  })
+})
+
+test('existing file expands to self (sync)', function (t) {
+  tmp.file(function (err, file) {
+    t.ok(!err)
+    t.equal(readlink.sync(file), file)
+    t.end()
   })
 })
 
@@ -47,6 +67,18 @@ test('symbolic link expands to same as fs.readlink()', function (t) {
           t.end()
         })
       })
+    })
+  })
+})
+
+test('symbolic link expands to same as fs.readlinkSync()', function (t) {
+  tmpdir(function (err, dir) {
+    t.ok(!err)
+    var symlink = path.join(dir, 'symlink')
+    fs.symlink(dir, symlink, function (err) {
+      t.ok(!err)
+      t.equal(fs.readlinkSync(symlink), readlink.sync(symlink))
+      t.end()
     })
   })
 })
@@ -71,6 +103,28 @@ test('embedded symbolic link expands to real path', function (t) {
           t.equal(result, dir2)
           t.end()
         })
+      })
+    })
+  })
+})
+
+test('embedded symbolic link expands to real path (sync)', function (t) {
+  // dir1     == /tmp/A
+  // dir2     == /tmp/B
+  // symlink  == /tmp/A/symlink -> /tmp
+  // symlink2 == /tmp/A/symlink/B
+  // test symlink2 expands to /tmp/B
+  tmpdir(function (err, dir1) {
+    t.ok(!err)
+    tmpdir(function (err, dir2) {
+      t.ok(!err)
+      var basename = path.basename(dir2)
+      var symlink = path.join(dir1, 'symlink')
+      fs.symlink('/tmp', symlink, function (err) {
+        t.ok(!err)
+        var symlink2 = path.join(symlink, basename)
+        t.equal(readlink.sync(symlink2), dir2)
+        t.end()
       })
     })
   })
